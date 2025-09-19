e7ea9eac9a0e0287190a0b6649bfc0c82202f065
PhucThinh2002 <137489705+PhucThinh2002@users.noreply.github.com>
2025-09-19 13:51:47 +0700

    Add test function for PR review (#5)
    * Add test function for PR review
    
    * Add input validation to test function

---
+++ js/test.js
@@ -0,0 +1,9 @@
+function testReview() { console.log('This is a test for PR review'); }
+
+function testReview(input) {
+  if (input) {
+    console.log('Test passed with input: ' + input);
+  } else {
+    console.log('Test failed: No input');
+  }
+}
