4f236ac9856108d0e0770755a4c7b6452e2eb9ee
thinh <duongphucthinh300702@gmail.com>
2025-09-19 14:59:17 +0700

    Add type validation

--- js/test.js
+++ js/test.js
@@ -1,7 +1,7 @@
 function testReview() { console.log('This is a test for PR review'); }
 
 function testReview(input) {
-  if (input) {
+  if (typeof input === 'string') {
     console.log('Test passed with input: ' + input);
   } else {
     console.log('Test failed: No input');
